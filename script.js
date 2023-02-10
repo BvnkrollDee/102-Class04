812c8138f601bc199b59298fca8621c5a17bd34d
Daron Faulkner Jr <dj200099@gmail.com>
2023-02-10 10:53:33 -0600

    htmlupdate

---
+++ script.js
@@ -0,0 +1,24 @@
+let a = 1;
+
+function l2(){}
+let lesson = function (x) {
+    let b = 5;
+    console.log(a);
+    console.log(b + a);
+
+}
+
+lesson(a)
+
+console.log(b);
+
+
+
+
+function whodat(){
+    
+    console.log("What is your name");
+    let name = prompt ("What is your name")
+    //let name = console.Readline();
+    console.log("Hello, " + name);
+}
